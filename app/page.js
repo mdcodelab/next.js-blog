b99983c93017692dd49dda35a45ac89554992790
miha11-hash <d_mihaela@msn.com>
2024-12-22 14:33:45 +0200

    images

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import News from './(components)/News'
+function Home() {
+  return (
+    <div>
+      <h1>App page</h1>
+      <News></News>
+      <input type="datw"></input>
+    </div>
+  )
+}
+
+export default Home
+

11a8824413fbd3084b1a4ea805fdc79d271583ea
miha11-hash <d_mihaela@msn.com>
2024-12-28 12:20:53 +0200

    header element

--- app/(components)/News.jsx
+++ app/(components)/News.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import Weather from "./Weather";
 import Calendar from "./Calendar";
 import RadioPlayer from "./RadioPlayer";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 function News() {
   return (
     <div className="news">
       {/* Header Section */}
       <header className="news-header">
-        <h1>News Header</h1>
+        <h1 className="logo">News & Blogs</h1>
+        <div className="search-bar">
+          <form>
+            <input type="text" placeholder="Search news..."></input>
+            <button type="submit"><FaMagnifyingGlass></FaMagnifyingGlass></button>
+          </form>
+        </div>
       </header>
 
       {/* Content Section */}

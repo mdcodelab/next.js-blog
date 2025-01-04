2fc10e4a3abbef8b2e7bc858dc68835c065f30b5
miha11-hash <d_mihaela@msn.com>
2025-01-04 05:39:54 +0200

    nagigating between News & Blogs

--- app/(components)/Blogs.jsx
+++ app/(components)/Blogs.jsx
@@ -1,32 +1,52 @@
+"use client";
 import React from 'react';
+import { useState } from 'react';
 
 function Blogs({onShowNews}) {
+  const [showForm, setShowForm]=useState(false);
+
   return (
     <div className="blogs">
-
       <div className="blogs-left">
         <img src="/images/me.jpg" title="mihaela"></img>
       </div>
 
       <div className="blogs-right">
-      <div className="blogs-right-form">
-        <h1>New Post</h1>
-        <form>
-          <div className="image-upload">
-            <label htmlFor="file-upload" className="file-upload">
-            <i className="bx bx-upload"></i> Upload Image</label>
-            <input type="file" id="file-upload"></input>
+        {showForm ? (
+          <div className="blogs-right-form">
+            <h1>New Post</h1>
+            <form>
+              <div className="image-upload">
+                <label htmlFor="file-upload" className="file-upload">
+                  <i className="bx bx-upload"></i> Upload Image
+                </label>
+                <input type="file" id="file-upload"></input>
+              </div>
+              <input
+                type="text"
+                placeholder="Add title (max 60 characters)"
+                className="title-input"
+              ></input>
+              <textarea
+                className="text-input"
+                placeholder="Add text..."
+              ></textarea>
+              <button type="submit" className="submit-btn">
+                Submit
+              </button>
+            </form>
           </div>
-          <input type="text" placeholder="Add title (max 60 characters)" className="title-input"></input>
-          <textarea className="text-input" placeholder="Add text..."></textarea>
-          <button type="submit" className="submit-btn">Submit</button>
-        </form>
-      </div>
-        {/* <button className="post-btn">Create New Post</button> */}
-        <button className="blogs-close-btn">Back <i className="bx bx-chevron-right" onClick={onShowNews}></i></button>
+        ) : (
+          <button className="post-btn" onClick={()=> setShowForm(true)}>Create New Post</button>
+        )}
+
+      
+        <button className="blogs-close-btn">
+          Back <i className="bx bx-chevron-right" onClick={onShowNews}></i>
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
 export default Blogs
